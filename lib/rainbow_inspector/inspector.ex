9325095f5635ba9c5b11a8ace109afe8716945b9
theAnnie <annie@pja.edu.pl>
2019-12-21 16:01:19 +0100

    allowed_colors are sorted now

--- lib/rainbow_inspector/inspector.ex
+++ lib/rainbow_inspector/inspector.ex
@@ -1,13 +1,13 @@
 defmodule RainbowInspector.Inspector do
   @allowed_colors ~w(
-    red
-    blue
-    green
-    yellow
-    white
-    magenta
-    cyan
     black
+    blue
+    cyan
+    green
+    magenta
+    red
+    white
+    yellow
     )a
 
   def color_inspect(item, font_color) when font_color in @allowed_colors do
@@ -16,7 +16,7 @@ defmodule RainbowInspector.Inspector do
   end
 
   def color_inspect(item, _font_color) do
-    IO.ANSI.reset
+    IO.ANSI.reset()
     IO.inspect(item)
   end
 end
